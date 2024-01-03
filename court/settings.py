d6cf4e27e21106168722174d90ac7aefff3320c8
Andrew Man Hay Chiu <amhchiu@gmail.com>
2024-01-04 09:26:08 +1100

    Remove whitenoise compression causing error

--- court/settings.py
+++ court/settings.py
@@ -124,12 +124,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
