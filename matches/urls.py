29ec2418afb40645d03e76c21f3ac3d666cdfe43
Andrew Man Hay Chiu <amhchiu@gmail.com>
2024-01-07 21:39:46 +1100

    Add scores to match tables and match edit page

--- matches/urls.py
+++ matches/urls.py
@@ -6,5 +6,5 @@ app_name = 'matches'
 urlpatterns = [
     path('', views.match_list, name='match'),
     path('add_match/', views.create_match_and_set, name='add_match'),
-    # path('match_set/<int:match_id>/', views.create_set, name='match_set')
+    path('edit_match/<int:match_id>', views.edit_match, name='edit_match'),
 ]
--- matches/views.py
+++ matches/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, get_list_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import MatchForm, SetForm
-from .models import Match
+from .models import Match, Set
 
 # Create your views here.
 def match_list(request):
@@ -34,3 +34,26 @@ def create_match_and_set(request):
     }
     
     return render(request, template_name='matches/create_match_set.html', context=context)
+
+@login_required
+def edit_match(request, match_id):
+    match = get_object_or_404(Match, id=match_id)
+    set = get_object_or_404(Set, match=match)
+
+    if request.method == 'POST':
+        match_form = MatchForm(request.POST, instance=match)
+        set_form = SetForm(request.POST, instance=set)
+        if match_form.is_valid():
+            match_form.save()
+            set_form.save()
+            return redirect('match_detail', id=match.id)
+    else:
+        match_form = MatchForm(instance=match)
+        set_form = SetForm(instance=set)
+    
+    context = {
+        'match_form': match_form,
+        'set_form': set_form
+    }
+    
+    return render(request, template_name='matches/edit_match.html', context=context)
